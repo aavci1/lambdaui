33ecbcada57644bd6e2e4c027c3a3fcc8ba8a202
Abdurrahman Avcı <abdurrahmanavci@gmail.com>
2026-06-30 16:46:00 +0300

    Clean up hello world demo

--- demos/hello-world/main.cpp
+++ demos/hello-world/main.cpp
@@ -1,31 +1,25 @@
 #include <Lambda.hpp>
-#include <Lambda/UI/Theme.hpp>
 #include <Lambda/UI/UI.hpp>
 
 using namespace lambdaui;
 
-struct HelloRoot {
-  auto body() const {
-    auto theme = useEnvironment<ThemeKey>();
-    return Text{
-        .text = "Hello, World!",
-        .font = Font::largeTitle(),
-        .color = Color::primary(),
-        .horizontalAlignment = HorizontalAlignment::Center,
-        .verticalAlignment = VerticalAlignment::Center,
-    };
-  }
-};
+int main(int argc, char *argv[]) {
+    Application app(argc, argv);
 
-int main(int argc, char* argv[]) {
-  Application app(argc, argv);
+    auto &w = app.createWindow({
+        .size = {400, 400},
+        .title = "Hello, World!",
+    });
 
-  auto& w = app.createWindow<Window>({
-      .size = {400, 400},
-      .title = "Hello, World!",
-  });
+    w.setView(
+        Text {
+            .text = "Hello, World!",
+            .font = Font::largeTitle(),
+            .color = Color::primary(),
+            .horizontalAlignment = HorizontalAlignment::Center,
+            .verticalAlignment = VerticalAlignment::Center,
+        }
+    );
 
-  w.setView<HelloRoot>();
-
-  return app.exec();
+    return app.exec();
 }
